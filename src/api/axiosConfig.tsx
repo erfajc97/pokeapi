1017bb6daeff7196f554872ea8b225d87294a39d
erfajc97 <erfacrypto@gmail.com>
2024-11-29 21:55:20 -0300

    [ADD] api url

--- src/api/axiosConfig.tsx
+++ src/api/axiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://pokeapi.co/api/v2/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json", // Default Content-Type
