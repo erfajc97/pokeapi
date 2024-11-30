1e5902e65a49cd556111832954c0825b898aaa7e
erfajc97 <erfacrypto@gmail.com>
2024-11-29 22:41:49 -0300

    [ADD] unit test with jest

--- src/__tests__/DetailPokemon.tsx
+++ src/__tests__/DetailPokemon.tsx
@@ -3,44 +3,31 @@ import DetailPokemon from "../features/PokemonDetails/DetailPokemon";
 import { usePokeApiStore } from "../store/pokeApiStore";
 
 jest.mock("next/navigation", () => ({
-  useRouter: jest.fn(() => ({
-    push: jest.fn(),
-  })),
+  useRouter: () => ({ push: jest.fn() }),
 }));
 
 jest.mock("../store/pokeApiStore");
 
 describe("DetailPokemon", () => {
-  beforeEach(() => {
-    (usePokeApiStore as unknown as jest.Mock).mockReturnValue({
-      pokemonDetail: {
-        name: "pikachu",
-        sprites: {
-          other: {
-            dream_world: {
-              front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
-            },
-          },
+  const mockPokemonDetail = {
+    name: "pikachu",
+    sprites: {
+      other: {
+        dream_world: {
+          front_default:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
         },
-        types: [
-          {
-            type: {
-              name: "electric",
-            },
-          },
-        ],
-        stats: [
-          {
-            base_stat: 35,
-            stat: {
-              name: "hp",
-            },
-          },
-        ],
-        height: 4,
-        weight: 60,
       },
+    },
+    types: [{ type: { name: "electric" } }],
+    stats: [{ base_stat: 35, stat: { name: "hp" } }],
+    height: 4,
+    weight: 60,
+  };
+
+  beforeEach(() => {
+    jest.mocked(usePokeApiStore).mockReturnValue({
+      pokemonDetail: mockPokemonDetail,
       isLoading: false,
       getDetailPokemon: jest.fn(),
     });
